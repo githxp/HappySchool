75cd8c6c14e4646a6efdfa04a168899dc122d146
hxp <1131647068@qq.com>
2015-11-26 20:31:27 +0800

    主分支-第二次提交-地图出现-2015/11/26/20:30

--- app/src/main/java/com/hxp/happyschool/activitys/MainActivity.java
+++ app/src/main/java/com/hxp/happyschool/activitys/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hxp.happyschool.activitys;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.hxp.happyschool.R;
 
@@ -13,5 +16,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+        Button obj_navigation = (Button) findViewById(R.id.btn_navigation);
+        obj_navigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent6 = new Intent(MainActivity.this,Map3DActivity.class);
+                startActivity(intent6);
+            }
+        });
 }
 }
