1046897280a5630dcb92dab0456c8c9e30089fd0
hxp <1131647068@qq.com>
2015-11-26 19:32:54 +0800

    项目第一次提交-2015/11/26/19:32

---
+++ app/src/main/java/com/hxp/happyschool/activitys/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hxp.happyschool.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hxp.happyschool.R;
+
+/**
+ * Created by hxp on 15-11-26.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+}
+}
